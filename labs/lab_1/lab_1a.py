f482402e9635f6cb64f997a8a819c1e76e56260d
Gleydy D,R <gleydydiazrodriguez@gmail.com>
2026-02-24 16:06:04 -0500

    2/24/26
    Added a personal introduction. In Line 14, there is a new line of code. Print (""). Changed made around 4:05pm.

--- labs/lab_1/lab_1a.py
+++ labs/lab_1/lab_1a.py
@@ -8,10 +8,10 @@ with your name. Then, save the code, add it to the staging area, and commit it t
 def main():
     print("Hello World!")
 
-    name = "" # TODO: Insert your name between the double quotes
     name = "Gleydy" # TODO: Insert your name between the double quotes
 
     print(f"{name}, Welcome to the CSS course!")
+    print ("My name is Gleydy, a sophomore from New York. I have major interests in coding, the cybersecurity field, f1, chess, arts and animations. I play two instruments, the piccolo and the flute, currently having the interest to play other instruments as well, like the trumpet and violin. I love reading and writing, specifically about  philosophy and history. History/Government and Mathematics are the two major subjects I've like. I've gained the interest for coding after being exposed to it last year from a class took which was Website Development. My curiosity to know how operating systems work behind the scenes spark more after learning about the amount of security some websites have and how vulnerable they may be. I would like to figure out how to make things more secure. Outside of school, I would participate in Science competitions, learn new skills and refining the ones I have. I would like to be a cybersecurity expert and maybe work in the field of law on the side.")
 
 if __name__ == "__main__":
     main()
